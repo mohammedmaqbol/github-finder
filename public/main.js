811397c55f7f40e45750d3971004d7fb46450e64
Mohammed  Maqbol <mohammedmaqbol1@gmail.com>
2022-01-24 17:40:07 +0200

    Update main.js

--- public/main.js
+++ public/main.js
@@ -9,18 +9,39 @@ search.addEventListener("click", fetchData);
                     const res = await fetch(user)
                     .then((res)=> res.json())
                     .then((data) => { console.log(data);
-                        let tab =  
-                            `<tr> 
-                                <td>${data.name} </td><br>
-                                <td>${data.bio} </td><br>
-                                <img src=${data.avatar_url}>     
-                            </tr>`;
-                        document.getElementById("employees").innerHTML = tab;
+                        if(data.message == "Not Found"){
+                                    alert("User Not Found");
+                        }else{
+                            let tab =
+                                `<div class="row">
+                                    <div class="col-sm-6 col-md-4">
+                                    <img src=" ${data.avatar_url}" alt="" class="img-rounded img-responsive" />
+                                </div>
+                                <div class="col-sm-6 col-md-8">
+                                    <h4>${data.name}</h4>
+                                    <small><cite title="">${data.location} <i class="glyphicon glyphicon-map-marker"></i></cite></small>
+                                    <p>
+                                        <i class="glyphicon glyphicon-envelope">${data.bio}</i>
+                                        <br />
+                                        <br />
+                                        <i class="glyphicon glyphicon-gift"></i><a>${data.html_url}</a>
+                                    </p>
+                                </div>`;
+                            document.getElementById("employees").innerHTML = tab;
+
+                        }
+                    
                     })
                 }else{
                     alert("Enter User Name")
                 }
             }
+
+
+         
+
+           
+
 
 
          
